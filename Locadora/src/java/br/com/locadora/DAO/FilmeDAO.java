e87ef0d06256d0c3b55c6c5550dd8f8d3713232d
washingtonLuis <washington.luis@ixasolutions.com.br>
2015-07-30 09:52:43 -0300

    filme sugerir

--- Locadora/src/java/br/com/locadora/DAO/FilmeDAO.java
+++ Locadora/src/java/br/com/locadora/DAO/FilmeDAO.java
@@ -258,11 +258,11 @@ public class FilmeDAO {
         for(Ator ator : filme.getAtores()){
             atores += "|" + ator.getNome();
         }
-        atores = atores.replaceFirst("|", atores);
+        atores = atores.replaceFirst("|", "");
         
         StringBuilder sql = new StringBuilder();
         sql.append(" SELECT f.id id, f.titulo titulo, f.genero_enum genero, f.classificacao classif, f.preco preco FROM filme f ");
-            sql.append(" INNER JOIN f.filme_ator fa ON fa.filme_id = f.id INNER JOIN ator a on a.id = fa.ator_id ");
+            sql.append(" INNER JOIN filme_ator fa ON fa.filme_id = f.id INNER JOIN ator a on a.id = fa.ator_id ");
             sql.append(" where f.genero_enum = ? OR UPPER(a.nome) SIMILAR TO '%(" + atores + ")%'");
             sql.append(" GROUP BY f.id, f.titulo, f.genero_enum, f.classificacao, f.preco ");
         
