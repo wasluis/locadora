eb198a07d15c4370a9fc23954fa1efd772ccbfb7
Diego Gomes Tomé <diegogomest@gmail.com>
2015-08-27 01:25:42 -0300

    COmitando ainda a ligação.

--- Agenda/src/br/com/framework/dao/ChamadaDAOImpl.java
+++ Agenda/src/br/com/framework/dao/ChamadaDAOImpl.java
@@ -1,7 +1,7 @@
 package br.com.framework.dao;
 
-import br.com.framework.model.Contato;
+import br.com.framework.model.Chamada;
 
-public class ChamadaDAOImpl extends BaseDAOImpl<Contato> {
+public class ChamadaDAOImpl extends BaseDAOImpl<Chamada> {
 	
 }
