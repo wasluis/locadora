15d0fdb2c5e221bd082201281bd1a92eade835e3
Diego Gomes Tomé <diegogomest@gmail.com>
2015-08-27 00:52:54 -0300

    ChamadaVO

---
+++ Agenda/src/br/com/framework/vo/ChamadaVO.java
@@ -0,0 +1,23 @@
+package br.com.framework.vo;
+
+public class ChamadaVO {
+	String duracao;
+	Long contatoId;
+
+	public String getDuracao() {
+		return duracao;
+	}
+
+	public void setDuracao(String duracao) {
+		this.duracao = duracao;
+	}
+
+	public Long getContatoId() {
+		return contatoId;
+	}
+
+	public void setContatoId(Long contatoId) {
+		this.contatoId = contatoId;
+	}
+
+}
